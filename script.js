3f1ac271c4365e8a35366644e5fc82e1050b0541
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-12 23:56:06 +1030

    Added timer.

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let decreaseTime = 10;
 // total answers, index 0 correct answer, index 1 wrong answer
 let totalAnswers = [0, 0];
 
+// timerid
+let timerId=0;
+
+// current question number, we start from 0
+let currentQuestion = 0;
+
 // questions (some are taken from https://www.guru99.com/javascript-interview-questions-answers.html )
 let questionsArray = [
     "What is JavaScript?",
@@ -37,16 +43,60 @@ let correctAnswersArray = [
     "B - false"
 ]
 
-// questions -> we create array of objects "question"
+// questions - this is array of objects
 let questions = [];
 for (let i = 0; i < questionsArray.length; i++){
+    // create new object
     let question_object = new question(questionsArray[i],answersArray[i],correctAnswersArray[i]);
+    // push new object to array
     questions.push(question_object);
+    // debug
     console.log(questions[i].choices);
     console.log(questions[i].correctChoices);
 }
 
+function startQuiz() {
+
+    // enable stop button
+    document.querySelector("#stopButton").setAttribute("class","btn btn-dark active");
+    //start counting
+    document.querySelector("#timeLeft").textContent = quizTime;
 
 
+    timerId = setInterval(function() {
+        quizTime--;
+        
+    
+        // need to check <= 0, === 0 is not enough
+        if(quizTime <= 0) {
+          // stop the timer
+          clearInterval(timerId);
+        }
+        document.querySelector("#timeLeft").textContent = quizTime;       
+      }, 1000);
+
+    
 
 
+    console.log("start");
+}
+
+function stopQuiz() {
+    // disable buttons
+    document.querySelector("#startButton").setAttribute("class","btn btn-dark disabled");
+    document.querySelector("#submitButton").setAttribute("class","btn btn-dark disabled");
+
+    // stop timer
+    clearInterval(timerId);
+    quizTime = 1;
+    console.log("YOU GAVE UP!!!");
+}
+
+// here we add events
+//startButton
+//.addEventListener("click", function() {
+
+
+// Functions
+document.querySelector("#startButton").addEventListener("click",startQuiz);
+document.querySelector("#stopButton").addEventListener("click",stopQuiz);
